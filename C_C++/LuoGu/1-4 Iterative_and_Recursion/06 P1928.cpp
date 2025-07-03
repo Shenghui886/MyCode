229b6e9785ab10f6b40a0369fcbd59487aa1afc9
XiaoFeng <3198133420@qq.com>
2025-07-04 02:33:32 +0800

    Solved problem P1928 using recursion in C++

--- C_C++/LuoGu/1-4 Iterative_and_Recursion/06 P1928.cpp
+++ C_C++/LuoGu/1-4 Iterative_and_Recursion/06 P1928.cpp
@@ -1,24 +1,43 @@
 // P1928 外星密码
 // Use stack DAT
 
-#include<stdio.h>
+#include <iostream>
+using namespace std;
 
-int main(void) {
-
-    int count = 0, num = 0;
-    char ch = 0, arr[20000];
-    while((ch = getchar()) != EOF)
-    {
-        if(ch == '[')
-        {
-            count++;
-            while((ch = getchar()) != ']')
-                arr[num++] = ch;
-            arr[num] = '\0';
-            for(int i = 0; i < arr[0]; i++) 
-                fputs(arr + 1, stdout);
+string decode(const string &s, int &n) {
+    string result;
+    int repeat = 0;
+    for(; n < s.size(); n++) {
+        if(isalpha(s[n]))
+            result.push_back(s[n]);
+        else if(s[n] == '[') {
+            ++n;
+            result += decode(s, n);
+        }
+        else if(s[n] == ']') {
+            string temp = result;
+            for(int j = 1; j < repeat; j++) {
+                result += temp;
+            }
+            return result;
+        } else if(isdigit(s[n])) {
+            while(isdigit(s[n])) {
+                repeat = repeat * 10 + (s[n] - '0');
+                n++;
+            }
+            n--; // Adjust for the increment in the loop
         }
     }
+    return result;
+}
+
+int main(int argc, char** argv) {
+
+    string s;
+    cin >> s;
+    int n = 0;
+    string result = decode(s, n);
+    cout << result << endl;
 
     return 0;
 }
