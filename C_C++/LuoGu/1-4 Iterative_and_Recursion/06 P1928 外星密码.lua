166e3a911c39423a60c3813e07773317a78a7db8
XiaoFeng <3198133420@qq.com>
2025-07-07 23:03:41 +0800

    Solved problem P1928 in Lua. Learned generics and iterators in go.

--- C_C++/LuoGu/1-4 Iterative_and_Recursion/06 P1928 外星密码.lua
+++ C_C++/LuoGu/1-4 Iterative_and_Recursion/06 P1928 外星密码.lua
@@ -1,5 +1,48 @@
+local function decompress(str)
+	local result = {}
+	local i = 1
+	local n = #str
+	while i <= n do
+		local char = str:sub(i, i)
+		if char == '[' then
+			i = i + 1
+			local num_str = ""
+			while i <= n and str:sub(i, i):match("%d") do
+				num_str = num_str .. str:sub(i, i)
+				i = i + 1
+			end
+			local num = tonumber(num_str)
+			if not num then
+				error("Invalid number in brackets")
+			end
+			local count = 1
+			local j = i
+			while count > 0 and j <= n do
+				local next_char = str:sub(j, j)
+				if next_char == '[' then
+					count = count + 1
+				elseif next_char == ']' then
+					count = count - 1
+				end
+				j = j + 1
+			end
+			local inner_str = str:sub(i, j - 2) -- Exclude the closing bracket
+			local decompressed_inner = decompress(inner_str)
+			for _ = 1, num do
+				table.insert(result, decompressed_inner)
+			end
+			i = j -- Move past the closing bracket
+		else
+			table.insert(result, char)
+			i = i + 1
+		end
+	end
+	return table.concat(result)
+end
+
 local function main(...)
-	
+	local str = io.read("*l")
+	print(decompress(str))
 end
 
 -- Detect if executed directly or required
